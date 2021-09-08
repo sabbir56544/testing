836ffcf0ba7d895dffc65f58a8689ce8715c6fbb
Sabbir Ahmed <67659815+sabbir56544@users.noreply.github.com>
2021-09-09 00:33:21 +0600

    static commit

--- home/settings.py
+++ home/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
 
 
 MEDIA_URL = '/media/'
