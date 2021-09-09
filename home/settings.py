6dd3341afc38856f2584fb73e872eb8476b662f9
Sabbir Ahmed <67659815+sabbir56544@users.noreply.github.com>
2021-09-09 15:39:56 +0600

    second commit

--- home/settings.py
+++ home/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'ahadsite.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenose.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenose.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'home.urls'
 
