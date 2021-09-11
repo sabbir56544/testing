674235d177cb57b467f88c3bebf8381995af8916
Sabbir Ahmed <67659815+sabbir56544@users.noreply.github.com>
2021-09-12 01:08:08 +0600

    search commit

--- home/settings.py
+++ home/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 
 
 MEDIA_URL = '/media/'
